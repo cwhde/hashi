6378275d6bfa3d5a55486f2714355474c675d920
juzo <julian@juzo.io>
2026-06-05 20:39:09 +0200

    Fix blocklist lifecycle migration discovery

--- src/Hashi.Infrastructure/Persistence/Migrations/20260605120000_AddBlocklistLifecycleAndJobFields.cs
+++ src/Hashi.Infrastructure/Persistence/Migrations/20260605120000_AddBlocklistLifecycleAndJobFields.cs
@@ -1,4 +1,7 @@
 using System;
+using Hashi.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
@@ -6,6 +9,8 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace Hashi.Infrastructure.Persistence.Migrations
 {
     /// <inheritdoc />
+    [DbContext(typeof(HashiDbContext))]
+    [Migration("20260605120000_AddBlocklistLifecycleAndJobFields")]
     public partial class AddBlocklistLifecycleAndJobFields : Migration
     {
         /// <inheritdoc />
